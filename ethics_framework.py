8b7c68914ed3612ab36ee8a29ddc8269a8acb13a
Kaelisalive <kaelisnotalive@gmail.com>
2024-12-05 03:14:12 -0500

    Delete ethics_framework.py

--- ethics_framework.py
+++
@@ -1,7 +0,0 @@
-
-class EthicsFramework:
-    def __init__(self):
-        self.core_values = ["transparency", "respect", "adaptability"]
-
-    def check_ethics(self, decision):
-        return "Ethical" if all(value in decision for value in self.core_values) else "Unethical"
